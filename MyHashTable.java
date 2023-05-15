525924942732c86953781454dea10d56dabbcd69
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-05-15 14:51:06 +0600

    I added the scanner and function boolean

--- MyHashTable.java
+++ MyHashTable.java
@@ -4,3 +4,5 @@ public class Main {
     public static void main(String[] args) {
         MyHashTable<String, Integer> table = new MyHashTable<>();
 
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
