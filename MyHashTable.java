aba2295fb649bd62668f844df375273e2a211da2
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-05-15 14:53:28 +0600

    i added int choice

--- MyHashTable.java
+++ MyHashTable.java
@@ -11,3 +11,5 @@ public class Main {
             System.out.println("Enter your choice:");
             System.out.println("1. Put (key, value)\n2. Get value by key\n3. Remove by key\n4. Check if value exists\n5. Get key by value");
             System.out.println("6. Exit");
+
+            int choice = scanner.nextInt();
