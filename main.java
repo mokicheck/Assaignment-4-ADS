c82bde4171df8cef1682d8c51bcac2897bbc3d43
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-05-15 15:05:36 +0600

    until this time, i wrote the code to wrong class, so i transfer it ti his correct order (i am so stupid)

--- main.java
+++ main.java
@@ -2,8 +2,71 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        MyHashTable table = new MyHashTable();
+        MyHashTable<String, Integer> table = new MyHashTable<>();
 
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
+        while (!exit) {
+            System.out.println("Enter your choice:");
+            System.out.println("1. Put (key, value)\n2. Get value by key\n3. Remove by key\n4. Check if value exists\n5. Get key by value");
+            System.out.println("6. Exit");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter key:");
+                    String key = scanner.next();
+                    System.out.println("Enter value:");
+                    int value = scanner.nextInt();
+                    table.put(key, value);
+                    System.out.println("Value inserted!");
+                    break;
+                case 2:
+                    System.out.println("Enter key:");
+                    key = scanner.next();
+                    Integer result = table.get(key);
+                    if (result != null) {
+                        System.out.println("Value: " + result);
+                    } else {
+                        System.out.println("Key not found!");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter key:");
+                    key = scanner.next();
+                    Integer removedValue = table.remove(key);
+                    if (removedValue != null) {
+                        System.out.println("Value removed: " + removedValue);
+                    } else {
+                        System.out.println("Key not found!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter value:");
+                    value = scanner.nextInt();
+                    boolean contains = table.contains(value);
+                    System.out.println("Value exists: " + contains);
+                    break;
+                case 5:
+                    System.out.println("Enter value:");
+                    value = scanner.nextInt();
+                    String foundKey = table.getKey(value);
+                    if (foundKey != null) {
+                        System.out.println("Key: " + foundKey);
+                    } else {
+                        System.out.println("No such value.");
+                    }
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Goodbye!");
     }
 }
