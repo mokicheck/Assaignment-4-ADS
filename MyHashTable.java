e9fc5c40e598b3c4bfd2268edb6547c0a05243ec
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-05-15 14:50:33 +0600

    I have imported the Scanner class from the java.util package, indicating that I may want to take user input at some point in your program.

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,2 +1,6 @@
-public class MyHashTable {
-}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MyHashTable<String, Integer> table = new MyHashTable<>();
+
--- main.java
+++ main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        MyHashTable<String, Integer> table = new MyHashTable<>();
+        MyHashTable table = new MyHashTable();
 
 
+    }
 }
