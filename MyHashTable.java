a7fd14868e2adbaf669cd3ddc56d0a6f783d82ca
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-05-15 14:52:56 +0600

    I have created a loop that continues until the "exit" variable is set to true. Within the loop, me displaying a menu to the user and prompting them to enter their choice.

--- MyHashTable.java
+++ MyHashTable.java
@@ -9,3 +9,5 @@ public class Main {
 
         while (!exit) {
             System.out.println("Enter your choice:");
+            System.out.println("1. Put (key, value)\n2. Get value by key\n3. Remove by key\n4. Check if value exists\n5. Get key by value");
+            System.out.println("6. Exit");
